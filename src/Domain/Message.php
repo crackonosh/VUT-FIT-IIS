415f6d774b9ffbc3d543347e9d40694d256fb06c
Crackonosh <lukashais@getgoogleoff.me>
2021-10-21 18:17:49 +0200

    Add Message.php file for doctrine

---
+++ src/Domain/Message.php
@@ -0,0 +1,45 @@
+<?php
+namespace Domain;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Messages")
+ */
+class Message
+{
+    /**
+     * @ORM\ID
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Thread")
+     * @ORM\JoinColumn(onDelete="CASCADE")
+     * @var string
+     */
+    private $thread;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    private $text;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(onDelete="CASCADE")
+     * @var int
+     */
+    private $created_by;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     * @var DateTime
+     */
+    private $created_on;
+}
