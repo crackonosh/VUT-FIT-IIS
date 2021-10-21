4179c43c2aac4a2107870ca97d98b16c1808c778
Crackonosh <lukashais@getgoogleoff.me>
2021-10-21 19:54:10 +0200

    add constructor

--- src/Domain/User.php
+++ src/Domain/User.php
@@ -54,6 +54,18 @@ class User
      */
     private $role;
 
+    public function __construct(
+        string $name,
+        string $password,
+        string $email,
+        int $role
+    ){
+        $this->name = $name;
+        $this->password = $password;
+        $this->email = $email;
+        $this->role = $role;   
+    }
+
     public function getName(): string
     {
         return $this->name;
@@ -62,5 +74,55 @@ class User
     public function setName(string $name): void
     {
         $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    public function getRole(): int
+    {
+        return $this->role;
+    }
+
+    public function setRole(int $role): void
+    {
+        $this->role = $role;
     }
 }
