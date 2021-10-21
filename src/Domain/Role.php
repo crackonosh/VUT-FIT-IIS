58fd56a23c3ccf598cdc4b372f35b37670ad65e0
Crackonosh <lukashais@getgoogleoff.me>
2021-10-21 19:49:06 +0200

    add set/get methods

--- src/Domain/Role.php
+++ src/Domain/Role.php
@@ -27,4 +27,14 @@ class Role
     {
         $this->name = $name;
     }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
