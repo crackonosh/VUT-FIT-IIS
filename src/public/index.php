5df1d0dfe3bf2aa28638abea13a85ae36b6760fc
Crackonosh <lukashais@getgoogleoff.me>
2021-10-22 15:58:59 +0200

    update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,8 @@
 <?php
 
-use Domain\User;
+use App\Controller\UserController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Exception\HttpNotFoundException;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -12,36 +11,17 @@ require __DIR__ . '/../../vendor/autoload.php';
 AppFactory::setContainer(require __DIR__ . '/../../bootstrap.php');
 $app = AppFactory::create();
 
-/*
-$app->addRoutingMiddleware();
-
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-$errorMiddleware->setErrorHandler(
-    HttpNotFoundException::class,
-    function (Request $request, Throwable $e, bool $displayErrorDetails) {
-        $response = new Response();
-        $response->getBody()->write('404 NOT FOUND');
-
-        return $response->withStatus(404);
-    }
-);
-*/
-
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $myService = $this->get("settings")['doctrine']['metadata_dirs'];
     var_dump($myService);die();
 
-    $user = new User();
-    var_dump($user);
-    
-
     $response->getBody()->write(
-        //json_encode($myService)
         "kek"
     );
     return $response;
 });
+
+$app->get('/user', UserController::class . ':addUser');
 
 $app->run();
