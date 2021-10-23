006ab93bbf40b63543a71ed2816b8a4cdf6deb6f
Crackonosh <lukashais@getgoogleoff.me>
2021-10-23 09:07:04 +0200

    Added errorMiddleware and json parser middleware

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,7 @@
 use App\Controller\UserController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -11,17 +12,42 @@ require __DIR__ . '/../../vendor/autoload.php';
 AppFactory::setContainer(require __DIR__ . '/../../bootstrap.php');
 $app = AppFactory::create();
 
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setErrorHandler(
+    HttpNotFoundException::class,
+    function (Request $request, Throwable $e, bool $displayErrorDetails) {
+        $response = new Response();
+        $response->getBody()->write('404 NOT FOUND');
+        return $response->withStatus(404);
+    }
+);
+
+$mlwr = $app->addBodyParsingMiddleware(
+    ["string" => function (Request $request, RequestHandler $handler):Response
+    {
+        $contentType = $request->getHeaderLine('Content-Type');
+
+        if (strstr($contentType, 'application/json'))
+        {
+            $contents = json_decode(file_get_contents('php://input'), true);
+
+            if (json_last_error() === JSON_ERROR_NONE)
+                $request = $request->withParsedBody($contents);
+        }
+
+        return $handler->handle($request);
+    }]
+);
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $myService = $this->get("settings")['doctrine']['metadata_dirs'];
-    var_dump($myService);die();
-
     $response->getBody()->write(
         "kek"
     );
     return $response;
 });
 
-$app->get('/user', UserController::class . ':addUser');
+$app->post('/user', UserController::class . ':addUser');
+$app->get('/users', UserController::class . ':getUsers');
+
 
 $app->run();
