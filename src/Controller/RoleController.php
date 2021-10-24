4314c08c5ceda37e723d15c3d5236a0891730882
Crackonosh <lukashais@getgoogleoff.me>
2021-10-24 11:55:51 +0200

    update RoleController.php for Create,Read,Update

--- src/Controller/RoleController.php
+++ src/Controller/RoleController.php
@@ -11,7 +11,6 @@ require_once __DIR__ . '/../Functions.php';
 class RoleController
 {
     private $em;
-    private $errorMsg = "";
 
     public function __construct(EntityManager $em)
     {
@@ -27,8 +26,43 @@ class RoleController
         $this->em->persist($role);
         $this->em->flush();
 
-        $response->withStatus(201);
+        $response = $response->withStatus(201);
         $response->getBody()->write("Successfully created new role '$roleName'.");
+        return $response;
+    }
+
+    public function readRoles(Request $request, Response $response): Response
+    {
+        $roles = $this->em->getRepository("App\Domain\Role")->findAll();
+
+        $msg = array();
+        foreach ($roles as $role)
+        {
+            array_push($msg, $role->getName());
+        }
+
+        $response->getBody()->write(json_encode($msg));
+        return $response;
+    }
+
+    public function updateRole(Request $request, Response $response, $args): Response
+    {
+        $roleID = $args["id"];
+        $newName = $args["name"];
+
+        $role = $this->em->find("App\Domain\Role", $roleID);
+        
+        if ($role == NULL)
+        {
+            $response = $response->withStatus(404);
+            $response->getBody()->write("Unable to find role with specified ID.");
+            return $response;
+        }
+
+        $role->setName($newName);
+        $this->em->flush();
+
+        $response->getBody()->write("Role successfully updated.");
         return $response;
     }
 }
