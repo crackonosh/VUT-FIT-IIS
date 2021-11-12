a1274428936fcf58f9d68146f2f56d682660bc43
Crackonosh <lukashais@getgoogleoff.me>
2021-11-12 12:50:17 +0100

    update courseservices

--- fituska-api/src/Services/CourseService.php
+++ fituska-api/src/Services/CourseService.php
@@ -7,14 +7,14 @@ class CourseService
 {
     public static function isCodeUnique(EntityManager &$em, string $code): bool
     {
-        $result = $em->getRepository("App\Domain\Course")->findBy(array("code" => "$code"));
+        $result = $em->getRepository("App\Domain\Course")->findBy(array("code" => $code));
 
         return count($result);
     }
 
     public static function isCourseApproved(EntityManager &$em, string $code): bool
     {
-        $result = $em->getRepository("App\Domain\Course")->findBy(array("code" => "$code"));
+        $result = $em->getRepository("App\Domain\Course")->findBy(array("code" => $code));
 
         // code is unique so 
         return $result[0]->getApprovedOn() != null;
