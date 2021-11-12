299b49b12fecf949d118173c18dd8c4293c8ecc4
Crackonosh <lukashais@getgoogleoff.me>
2021-11-12 14:08:46 +0100

    add desired functions to App\Controller\ThreadCategory

--- fituska-api/src/Controller/ThreadCategoryController.php
+++ fituska-api/src/Controller/ThreadCategoryController.php
@@ -76,4 +76,80 @@ class ThreadCategoryController
         $response->getBody()->write("Successfully created new thread category.");
         return $response;
     }
+
+    public function readThreadCategories(Request $request, Response $response, $args): Response
+    {
+        $results = $this->em->getRepository("App\Domain\ThreadCategory")->findBy(array("course" => $args["code"]));
+
+        if (!count($results))
+        {
+            $response = $response->withStatus(404);
+            $response->getBody()->write("No thread categories found for given course code.");
+            return $response;
+        }
+
+        $msg = array();
+        /** @var ThreadCategory */
+        foreach ($results as $tCategory)
+        {
+            $tmp = array(
+                "id" => $tCategory->getID(),
+                "name" => $tCategory->getName()
+            );
+
+            array_push($msg, $tmp);
+        }
+
+        $response->getBody()->write(json_encode($msg));
+        return $response;
+    }
+
+    public function updateThreadCategory(Request $request, Response $response, $args): Response
+    {
+        $body = $request->getParsedBody();
+
+        $bodyArguments = array(
+            "name" => createArgument("string", $body["name"])
+        );
+
+        parseArgument($this->errorMsg, $bodyArguments);
+        echo($this->errorMsg);
+
+        /** @var ThreadCategory */
+        $tCategory = $this->em->getRepository(ThreadCategory::class)->find($args["id"]);
+
+        if (!$tCategory)
+        {
+            $response = $response->withStatus(404);
+            $response->getBody()->write("Unable to find thread category with given ID.");
+            return $response;
+        }
+
+        $tCategory->setName($body["name"]);
+
+        $this->em->persist($tCategory);
+        $this->em->flush();
+
+        $response->getBody()->write("Successfully updated name of category.");
+        return $response;
+    }
+
+    public function deleteThreadCategory(Request $request, Response $response, $args): Response
+    {
+        /** @var ThreadCategory */
+        $tCategory = $this->em->getRepository(ThreadCategory::class)->find($args["id"]);
+
+        if (!$tCategory)
+        {
+            $response = $response->withStatus(404);
+            $response->getBody()->write("Unable to delete not existing category.");
+            return $response;
+        }
+
+        $this->em->remove($tCategory);
+        $this->em->flush();
+
+        $response->getBody()->write("Successfully deleted thread category.");
+        return $response;
+    }
 }
