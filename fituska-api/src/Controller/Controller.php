700891dc34bf9883670a951dae6a614daeb2b9f0
Crackonosh <lukashais@getgoogleoff.me>
2021-11-16 16:23:47 +0100

    add abstract class Controller

---
+++ fituska-api/src/Controller/Controller.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controller;
+
+abstract class Controller
+{
+    protected function createArgument(string $expectedType, &$argument, bool $isOptional = false): array
+    {
+        return array(
+            "expectedType" => $expectedType,
+            "value" => $argument,
+            "optional" =>$isOptional
+        );
+    }
+
+    protected function parseArgument(string &$error, $arguments): void
+    {
+        foreach ($arguments as $argName => $arg) {
+            // continue if expected type is equal to real type
+            if ($arg["expectedType"] == gettype($arg["value"])) continue;
+            
+            // continue if value is NULL and is optional
+            if ($arg["value"] == NULL && $arg["optional"]) continue;
+            
+            $error .= "Argument '$argName' expected types are: '" . $arg["expectedType"] . "', but '" . gettype($arg["value"]) . "' given.\n";
+        }
+    }
+}
